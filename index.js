e4ba5f64e6607e6f3b7dcd6facb3dfa7344f8f00
Dennis <dennismusembi344@gmail.com>
2024-09-13 12:52:26 +0300

    redone

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 function scuberGreetingForFeet(feet){
   // Write your code here!
-  let result;
+  // let result;
 
-  if (feet<=400){
-    result= "This one is on me!"
+  // if (feet<=400){
+  //   result= "This one is on me!"
+  // }
+  //   else if(feet>2000 && feet<=2500){
+  //     result= "I will gladly take your thirty bucks."
+  //   }
+  //   else if (feet>2500){
+  //     result = "No can do."
+  //   }
+  //   return result
+  let result
+  if(feet<=400){
+    result="This one is on me!"
   }
-    else if(feet>2000 && feet<=2500){
-      result= "I will gladly take your thirty bucks."
-    }
-    else if (feet>2500){
-      result = "No can do."
-    }
-    return result
-
+  else if(feet>2000 && feet<=2500){
+    result="I will gladly take your thirty bucks."
+  }
+  else if(feet>2500){
+    result="No can do."
+  }
+  
+  return result
   }
 
 
 function ternaryCheckCity(city){
   // Write your code here!
-  return city ==="NYC"?"Ok, sounds good.":"No go."
+  // return city ==="NYC"?"Ok, sounds good.":"No go."
+  return city==="NYC"?"Ok, sounds good.":"No go."
+   
 
 }
 
